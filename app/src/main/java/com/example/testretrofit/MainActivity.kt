81576f456a514f30e353434a4b15a3b9eb98e062
Rohit Singla <rohitdeepu17@gmail.com>
2023-09-04 17:15:30 +0530

    added all code for testing retrofit

--- app/src/main/java/com/example/testretrofit/MainActivity.kt
+++ app/src/main/java/com/example/testretrofit/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.testretrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val quotesAPI = RetrofitHelper.getInstance().create((QuotesAPI::class.java))
+
+        GlobalScope.launch {
+            val result = quotesAPI.getQuotes(1)
+            print(result.body())
+            Log.d("Rohit", "body : ${result.body()?.count}")
+        }
     }
 }
---
+++ app/src/main/java/com/example/testretrofit/QuotesAPI.kt
@@ -0,0 +1,10 @@
+package com.example.testretrofit
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface QuotesAPI {
+    @GET("/quotes")
+    suspend fun getQuotes(@Query("page")page: Int): Response<QuoteList>
+}
