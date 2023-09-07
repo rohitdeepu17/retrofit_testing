07638216842cd7427c508d3279b67d20e091342c
Rohit Singla <rohitdeepu17@gmail.com>
2023-09-07 12:00:00 +0530

    log interceptor

--- app/src/main/java/com/example/testretrofit/RetrofitHelper.kt
+++ app/src/main/java/com/example/testretrofit/RetrofitHelper.kt
@@ -1,6 +1,7 @@
 package com.example.testretrofit
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -10,8 +11,13 @@ object RetrofitHelper {
     private const val BASE_URL = "http://34.31.250.153:58001/"
     //private const val BASE_URL = "http://35.194.28.62:58001/"
 
+    val logging = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    //logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
     val client = OkHttpClient.Builder()
         .addInterceptor(BasicAuthInterceptor("test", "fitpeo"))
+        .addInterceptor(logging)
         .build()
 
     fun getInstance(): Retrofit
